3718902d584074088d6c7b8124c73861f7e75127
Jhei <jsalvatierra.work@gmail.com>
2026-03-14 16:23:43 +0800

    w02 learning activity quiz

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._startYear = 2022;
+        job1._endYear = 2025;
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Valve";
+
+        Job job2 = new Job();
+        job2._startYear = 2019;
+        job2._endYear = 2021;
+        job2._jobTitle = "Customer Service";
+        job2._company = "Meta";
+
+        Resume resume = new Resume();
+        resume._name = "Jhei Salvatierra";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.DisplayResume();
     }
 }
