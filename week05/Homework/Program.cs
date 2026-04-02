1c8e01d0ddf3b3d44358ac049108ad1283b48b44
Jhei <jsalvatierra.work@gmail.com>
2026-04-02 11:37:50 +0800

    homework w05 fin

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a = new("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment m = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(m.GetSummary());
+        Console.WriteLine(m.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment w = new WritingAssignment("Mary Waters","European History", "The Causes of World War II");
+        Console.WriteLine(w.GetSummary());
+        Console.WriteLine(w.GetWritingInformation());
     }
 }
