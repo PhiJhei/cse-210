e14bec44bac209b9dcc6a6a8db99de84f94eb315
Jhei <jsalvatierra.work@gmail.com>
2026-04-15 20:15:23 +0800

    unfinished, im lost and out of time.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Sandbox Project.");
+        public abstract class Object
+        {
+            protected string _shape;
+
+            public abstract void SetShape();
+        }
+
+        public class Wheel : Object
+        {
+            public override void SetShape()
+            {
+                _shape = "circle";
+            }
+        }
     }
 }
